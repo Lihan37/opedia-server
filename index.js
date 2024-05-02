76ae185eda82f9e6e94919e4d46e8b1a39748af3
eanur rahman <lihan.lihu@gmail.com>
2024-05-03 03:50:20 +0600

    updated some features

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const path = require("path");
 const jwt = require("jsonwebtoken");
 const rateLimit = require("express-rate-limit");
 const { body, validationResult } = require("express-validator");
@@ -13,7 +12,7 @@ const sanitizeHtml = require("sanitize-html");
 // Middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://opedia-blogs-eanur.netlify.app"],
     credentials: true,
   })
 );
@@ -29,20 +28,20 @@ app.use(limiter);
 
 const verifyToken = (req, res, next) => {
   if (!req.headers.authorization) {
-    return res.status(401).send({ message: "unauthorized access" });
+    return res.status(401).send({ message: "Unauthorized access" });
   }
 
   const token = req.headers.authorization.split(" ")[1];
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
     if (err) {
-      return res.status(401).send({ message: "forbidden access" });
+      return res.status(401).send({ message: "Forbidden access" });
     }
     req.decoded = decoded;
     next();
   });
 };
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.g9xsrko.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.g9xsrko.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -58,6 +57,7 @@ async function run() {
 
     const usersCollection = client.db("blogsDB").collection("users");
     const blogsCollection = client.db("blogsDB").collection("blogs");
+    const commentsCollection = client.db("blogsDB").collection("comments");
 
     app.post("/jwt", async (req, res) => {
       const user = req.body;
@@ -67,6 +67,7 @@ async function run() {
       res.send({ token });
     });
 
+    // Users Endpoints
     app.get("/users", verifyToken, async (req, res) => {
       const result = await usersCollection.find().toArray();
       res.send(result);
@@ -99,6 +100,7 @@ async function run() {
       }
     );
 
+    // Blogs Endpoints
     app.post("/blogs", async (req, res) => {
       try {
         const { title, content, authorEmail, thumbnail } = req.body;
@@ -155,12 +157,87 @@ async function run() {
         const { title, content } = req.body;
         await blogsCollection.updateOne(
           { _id: id },
-          { $set: { title: sanitizeHtml(title), content: sanitizeHtml(content) } }
+          {
+            $set: {
+              title: sanitizeHtml(title),
+              content: sanitizeHtml(content),
+            },
+          }
         );
         res.status(200).send();
       } catch (error) {
         console.error("Error editing blog:", error);
         res.status(500).json({ error: "Failed to edit blog" });
+      }
+    });
+
+    // Comments Endpoints
+
+    app.get("/comments", async (req, res) => {
+      const result = await commentsCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/blogs/:id/comments", verifyToken, async (req, res) => {
+      try {
+        const { content } = req.body;
+        const { id } = req.params;
+        const { email } = req.decoded; // Extract email from decoded token
+    
+        // Create the comment with the extracted email
+        const comment = {
+          content: sanitizeHtml(content),
+          blogId: id,
+          authorEmail: email, // Use extracted email as authorEmail
+          createdAt: new Date(),
+        };
+    
+        // Insert the comment into the database
+        const result = await commentsCollection.insertOne(comment);
+        comment._id = result.insertedId;
+        res.status(201).json(comment);
+      } catch (error) {
+        console.error("Error creating comment:", error);
+        res.status(500).json({ error: "Failed to create comment" });
+      }
+    });
+
+    app.get("/blogs/:id/comments", async (req, res) => {
+      try {
+        const { id } = req.params;
+        const comments = await commentsCollection
+          .find({ blogId: id })
+          .toArray();
+        res.json(comments);
+      } catch (error) {
+        console.error("Error fetching comments:", error);
+        res.status(500).json({ error: "Failed to fetch comments" });
+      }
+    });
+
+    app.put("/comments/:id", verifyToken, async (req, res) => {
+      try {
+        const { id } = req.params;
+        const { content } = req.body;
+        await commentsCollection.updateOne(
+          { _id: new ObjectId(id) },
+          { $set: { content: sanitizeHtml(content) } }
+        );
+        res.status(200).send();
+      } catch (error) {
+        console.error("Error editing comment:", error);
+        res.status(500).json({ error: "Failed to edit comment" });
+      }
+    });
+
+    app.delete("/comments/:id", verifyToken, async (req, res) => {
+      try {
+        const { id } = req.params;
+        await commentsCollection.deleteOne({ _id: new ObjectId(id) });
+        res.status(204).send();
+      } catch (error) {
+        console.error("Error deleting comment:", error);
+        res.status(500).json({ error: "Failed to delete comment" });
       }
     });
 
